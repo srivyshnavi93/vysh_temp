a81a03e17e04ff3fcd967961da14577f26ae409a
srivyshnavi93 <29574165+srivyshnavi93@users.noreply.github.com>
2023-01-15 21:42:18 -0600

    Update spark_example.py
    Added spark conf files

--- python_container/spark_example.py
+++ python_container/spark_example.py
@@ -1,4 +1,4 @@
 import pyspark
-spark = pyspark.sql.SparkSession.builder.appName("spark-sample").getOrCreate()
-sample = spark.range(1000 * 1000 * 1000).count()
+spark = pyspark.sql.SparkSession.builder.master("spark://127.0.0.1:7077").appName("spark-sample").getOrCreate()
+sample = spark.range(100000 + 1).count()
 print(sample)
