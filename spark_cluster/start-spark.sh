4aa3aa2430133b753e4852c45ecf5161d50b5fa0
srivyshnavi93 <29574165+srivyshnavi93@users.noreply.github.com>
2023-01-20 01:45:22 -0600

    working prototype

--- spark_cluster/start-spark.sh
+++ spark_cluster/start-spark.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-echo $SPARK_MASTER_HOST
-. "${SPARK_HOME}/bin/load-spark-env.sh"
+
+. "$SPARK_HOME/bin/load-spark-env.sh"
 
 echo $SPARK_MASTER_HOST
 # When the spark work_load is master run class org.apache.spark.deploy.master.Master
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
 
-/opt/spark/sbin/start-master.sh >> /opt/spark/logs/spark-master.out
+$SPARK_HOME/sbin/start-master.sh >> $SPARK_HOME/logs/spark-master.out 
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
 # When the spark work_load is worker run class org.apache.spark.deploy.master.Worker
-/opt/spark/sbin/start-worker.sh spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} >> /opt/spark/logs/spark-worker.out
+$SPARK_HOME/sbin/start-worker.sh spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} >> $SPARK_HOME/logs/spark-worker.out
+
+elif [ "$SPARK_WORKLOAD" == "history" ];
+then
+# When the spark work_load is worker run class org.apache.spark.deploy.master.Worker
+$SPARK_HOME/sbin/start-history-server.sh
 
 elif [ "$SPARK_WORKLOAD" == "submit" ];
 then
