8f03873e0295ebc60eb9ecf0b12d892aa19b21c2
srivyshnavi93 <29574165+srivyshnavi93@users.noreply.github.com>
2023-01-21 13:34:27 -0600

    Added 2 workers and notes

--- python_container/spark_example.py
+++ python_container/spark_example.py
@@ -1,5 +1,5 @@
 import pyspark
 # spark = pyspark.sql.SparkSession.builder.master("spark://127.0.0.1:7077").appName("spark-sample").getOrCreate()
-spark = pyspark.sql.SparkSession.builder.appName("spark-sample").getOrCreate()
+spark = pyspark.sql.SparkSession.builder.master("spark://172.17.0.2:7077").appName("spark-sample").getOrCreate()
 sample = spark.range(10000000 + 1).count()
 print(sample)
