d79d7db4e1566877c15144c9d907e2588439f6d1
srivyshnavi93 <29574165+srivyshnavi93@users.noreply.github.com>
2023-02-22 20:46:50 -0600

    Squashed commit of the following:
    commit 0353f188b76ca2fa3079f4aff149de4916123915
    Merge: e5f7f1e b08ccd0
    Author: srivyshnavi93 <29574165+srivyshnavi93@users.noreply.github.com>
    Date:   Wed Feb 22 20:05:40 2023 -0600
    
        Merge pull request #14 from srivyshnavi93/manoj-dev
    
        add thrift-server and superset
    
    commit b08ccd09c044309e5c5715b2d8ee18eb22969209
    Author: Manoj Mukkamala <manoj@shipt.com>
    Date:   Wed Feb 22 18:21:35 2023 -0600
    
        Update hive-site.xml
    
    commit 79e170f8f59625857d4033e8575dab9f6eed6b44
    Author: Manoj Mukkamala <manoj@shipt.com>
    Date:   Tue Feb 21 21:55:49 2023 -0600
    
        cleanup
    
    commit fc753076d872cd43b3561eef0d3bd64a1f5216e1
    Author: Manoj Mukkamala <manoj@shipt.com>
    Date:   Sat Feb 18 21:44:17 2023 -0600
    
        add thrift-server and superset

--- kafka_client_python/consumer.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from json import dumps, loads
-from kafka import KafkaConsumer
-
-my_consumer = KafkaConsumer('test-topic-1', bootstrap_servers = ['kafka-broker:9092'], auto_offset_reset = 'earliest', enable_auto_commit = True, group_id = 'test-consumer-group-1', value_deserializer = lambda x : loads(x.decode('utf-8')))
-
-for m in my_consumer:
-        print(m.value)
---
+++ kafka_clients/producer.py
@@ -0,0 +1,15 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+import random
+
+my_producer = KafkaProducer(bootstrap_servers = ['kafka-broker:9092'], value_serializer = lambda x:dumps(x).encode('utf-8'))
+
+# for n in range(200, 250):
+while True:
+    n = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+    my_data = {'data' : str(n)}
+    print(my_data)
+    my_producer.send(topic = 'test-topic-1', value = my_data)
+    sleep(1)
